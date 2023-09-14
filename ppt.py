4017b2fed603def04aa32179b52d43193f280176
Jesus Antonio Zuñiga Arce <jzunigarce@gmail.com>
2023-09-13 19:43:57 -0700

    feat(exit option)
    Se agrega una opción para volver a jugar o salir del juego

--- ppt.py
+++ ppt.py
@@ -1,9 +1,13 @@
+'''
+    Juego Piedra, Papel o Tijera
+    Las opciones a seleccionar son:
+    0 - Piedra
+    1 - Papel
+    2 - Tijera
+'''
 import random
 
-#0 - Piedra
-#1 - Papel
-#2 - Tijera
-
+#Devuelve el nombre de la opción seleccionada
 def numero_a_nombre(numero):
     if numero == 0:
         return "Piedra"
@@ -14,18 +18,10 @@ def numero_a_nombre(numero):
     else:
         return "Número fuera de rango"
 
-def nombre_a_numero(nombre):
-    if nombre == "Piedra":
-        return 0
-    elif nombre == "Papel":
-        return 1
-    elif nombre == "Tijera":
-        return 2
-    else:
-        return -1
-
 def juego(opcion_jugador):
+    #Genera una opción aleatoria con rango de 0-2
     opcion_computadora = random.randrange(0, 3)
+
     diferencia = (opcion_jugador - opcion_computadora) % 3
     
     if diferencia == 0:
@@ -38,14 +34,17 @@ def juego(opcion_jugador):
     nombre_computadora = numero_a_nombre(opcion_computadora)
     nombre_jugador = numero_a_nombre(opcion_jugador)
 
-    print(f"El usuario seleccionó {nombre_jugador}")
-    print(f"La computadora seleccionó {nombre_computadora}")
+    print("El usuario seleccionó {}".format(nombre_jugador))
+    print("La computadora seleccionó {}".format(nombre_computadora))
     print(resultado)
     
 
 def inicio():
-    opcion = int(input("0.-Piedra\n1.-Papel\n2.-Tijera\nSeleccione una opción: "))
-    juego(opcion)
-
+    while True:
+        opcion = int(input("0.-Piedra\n1.-Papel\n2.-Tijera\nSeleccione una opción: "))
+        juego(opcion)
+        opcion_juego = int(input("\n1.- Jugar de nuevo\n2.-Salir\nSeleccione una opción: "))
+        if opcion_juego == 2:
+            break
 
 inicio()
